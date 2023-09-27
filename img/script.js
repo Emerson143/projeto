9079e4caa8a6685b9aca5142ab33ac5df08cb9a8
Emerson143 <emerson.143@hotmail.com>
2023-09-27 07:53:05 -0300

    teste

---
+++ img/script.js
@@ -0,0 +1,13 @@
+var url_atual = window.location.href;
+
+console.log(url_atual);
+
+const seleciona = document.querySelector(".h1");
+
+const elementoAtivo = document.querySelector(".ativo");
+
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
